daa01938153de7935b2991bae5f902f98b3af1c8
Mahdi A <81549809+Mahdi-Alizadeh96@users.noreply.github.com>
2023-01-16 17:43:08 +0330

    add first validation

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ app.use(bodyParser.json()); //For Json Response
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Cotent-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 })
 
 app.use("/feed", feedRoutes)
 
-app.listen(3000);
+app.listen(8080);
--- controllers/feedController.js
+++ controllers/feedController.js
@@ -1,13 +1,43 @@
+const { validationResult } = require("express-validator/check");
+
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({name: "Mahdi", City: "Tehran"})
+    res.status(200).json({  
+                            posts : [
+                                {
+                                    _id: "1",
+                                    title: "First Post", 
+                                    content: "Dummy content",
+                                    imageUrl: "/images/duck.jpg",
+                                    creator: {
+                                        name: "Haji"
+                                    },
+                                    createdAt: new Date()
+                                }
+                            ]
+                        })
 };
 
 exports.createPost = (req, res, next) => {
+
+    const errors = validationResult(req); 
+
+    if(!errors.isEmpty()) {
+        return res.status(422).json({message: 'Validation failed, entered data is incorrect.', errors: errors.array()});
+    }
+
     const title = req.body.title;
     const content = req.body.content;
 
     res.status(201).json({
         mwssage: 'Post created successfully',
-        post: {id: Math.round(Math.random() * 100), title, content}
+        post:   {
+                    _id: Math.round(Math.random() * 100),
+                    title,
+                    content,
+                    creator: {
+                        name: 'Maximilian'
+                    },
+                    createdAt: new Date()
+                }
     })
 }
